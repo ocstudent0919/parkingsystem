41b9283daf1a1a435080385cf9331bddaeb39663
ocstudent0919 <ocstudent0919@protonmail.com>
2021-03-04 20:03:23 +0000

    Function first 30 minutes gratis added

---
+++ src/main/java/com/parkit/parkingsystem/constants/Promo.java
@@ -0,0 +1,5 @@
+package com.parkit.parkingsystem.constants;
+
+public class Promo {
+    public static final double PARKING_TIME_GRATIS = 0.5; // free parking time in hours
+}
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,6 +1,7 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.Promo;
 import com.parkit.parkingsystem.model.Ticket;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -32,7 +33,11 @@ public class FareCalculatorService {
     private double getDuration(long inTime, long outTime){
         //Convert duration in milliseconds to duration in hours (decimal value)
         double durationInHours = (double)(outTime - inTime) / (60 * 60 * 1000);
-        return roundValue(durationInHours);
+        double duration = roundValue(durationInHours);
+        //Exclude free parking time from duration;
+        if(duration <= Promo.PARKING_TIME_GRATIS)
+            return 0;
+        return duration;
     }
 
     private double roundValue(Double valueToRound){
