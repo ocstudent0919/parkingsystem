c1c804cebe1823758d99dbef4f7b9dc77c10963f
ocstudent0919 <ocstudent0919@protonmail.com>
2021-03-14 12:48:54 +0000

    Feature 5% discount: integration test added

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,6 +1,7 @@
 package com.parkit.parkingsystem.integration;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.constants.Promo;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -84,5 +85,28 @@ public class ParkingDataBaseIT {
         assertEquals(Fare.CAR_RATE_PER_HOUR, ticketDAO.getTicket("ABCDEF").getPrice());
         //Check that the out time is populated correctly in the database
         assertNotNull(ticketDAO.getTicket("ABCDEF").getOutTime(), "Out time was not set correctly");
+    }
+
+    @Test
+    public void testParkingACarRecurrentVisit() {
+        //GIVEN
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        //WHEN
+        parkingService.processIncomingVehicle(); // first time parking "ABCDEF" car
+        DateTimeUtils.setCurrentMillisOffset(3600000); // parking duration of 1 hour
+        parkingService.processExitingVehicle();
+        DateTimeUtils.setCurrentMillisOffset(4200000); // 10 minutes break between two visits
+        parkingService.processIncomingVehicle(); // second time parking "ABCDEF" car
+        DateTimeUtils.setCurrentMillisOffset(7800000); // parking duration of 1 hour
+        parkingService.processExitingVehicle();
+
+        //THEN
+        //Check that the ticket considered for a discount is the latest one
+        assertEquals(2, ticketDAO.getTicket("ABCDEF").getId(), "Wrong ticket considered");
+        //Check that the discount condition (recurrent visit) is satisfied
+        assertTrue(ticketDAO.checkDiscountEligibility("ABCDEF"), "Discount eligibility check failed");
+        //Check that the generated fare is populated correctly in the database
+        assertEquals(Fare.CAR_RATE_PER_HOUR * (1 - Promo.REDUCTION_RATE), ticketDAO.getTicket("ABCDEF").getPrice(), "Discount application failed");
     }
 }
